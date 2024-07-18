41bea33dbb09b7eaf682ad2099822a05714ff16a
unknown <jasoncm712@gmail.com>
2024-07-18 15:01:58 +0700

    feat: splash screen

--- app/src/main/java/com/example/jancook/MainActivity.java
+++ app/src/main/java/com/example/jancook/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.jancook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
     }
 }
